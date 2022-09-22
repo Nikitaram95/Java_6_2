895cc218980df61580863181d1faa045de5f4f1e
Nikitaram95 <105075205+Nikitaram95@users.noreply.github.com>
2022-09-22 20:35:26 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,11 +1,11 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate(int x, int y) {
+    public int calculate(int min, int max) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
             int square = i * i;
-            if (square >= x && square <= y) {
+            if (square >= min && square <= max) {
                 count++;
             }
         }
